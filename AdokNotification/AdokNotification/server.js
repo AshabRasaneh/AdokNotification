5de59f878aab41a10b3b111a587f6aa6d3c5f38c
saeed <saeedasadsh@gmail.com>
2018-08-27 19:28:34 +0430

    123

--- AdokNotification/AdokNotification/server.js
+++ AdokNotification/AdokNotification/server.js
@@ -1028,7 +1028,7 @@ function GetNotificationsWeb() {
                     }
                 }
 
-
+                console.log(buffer);
                 var row = JSON.parse(buffer);
 
                 for (var i = 0; i < row.length; i++) {

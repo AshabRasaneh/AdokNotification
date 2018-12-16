b4871444d5dcac834140eebebb34439b16035b81
saeed <saeedasadsh@gmail.com>
2018-12-16 11:42:13 +0330

    123

--- AdokNotification/AdokNotification/serverNet.js
+++ AdokNotification/AdokNotification/serverNet.js
@@ -64,7 +64,10 @@ function SetImAlive() {
         var dateHejri = GetCurrentDate()+""+GetCurrentTime();
         console.log("set state:"+dateHejri);
         var quer = "update adokState set notificationState="+ dateHejri+"where id=1";
-        con.query(quer,function (err, result, fields) {
+        con.query(quer, function (err, result, fields) {
+            console.log("err:" + err);
+            console.log("result:" + result);
+
             if (!err) {
             }
         });
